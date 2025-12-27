ae0574db09136cf0cf7996d33aabf5b2347f4b11
zabe <zmhyxd@gmail.com>
2025-12-27 21:02:15 +0300

    Update bank.c

--- bank.c
+++ bank.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-double withdraw(double amount,double withdraw_amount) {
+double withdraw(double amount, double withdraw_amount)
+{
     return amount - withdraw_amount;
 }
- void run_code () {
+void run_code()
+{
     double withdraw_amount;
     double account_balance;
     double balance;
-
+    char again;
 
     printf("Enter the the account balance:\n");
     scanf("%lf", &balance);
@@ -14,33 +16,48 @@ double withdraw(double amount,double withdraw_amount) {
     printf("Enter the amount you wish to withdraw:\n");
     scanf("%lf", &withdraw_amount);
 
-    if (withdraw_amount < 0) {
+    if (withdraw_amount < 0)
+    {
         printf("Invalid amount\n");
         return;
-
     }
-    if (withdraw_amount > balance) {
+    if (withdraw_amount > balance)
+    {
         printf("Insufficient funds\n");
         return;
-
     }
-    else if (withdraw_amount < -1) {
-       printf("Invalid amount\n");
-       return;
-
+    else if (withdraw_amount < -1)
+    {
+        printf("Invalid amount\n");
+        return;
     }
-    account_balance = withdraw(balance,withdraw_amount);
+    account_balance = withdraw(balance, withdraw_amount);
 
     printf("Withdrawal amount:$%.2lf\n", withdraw_amount);
     printf("Remaining balance:$%.2lf\n", account_balance);
 
+    
+    printf("Do you want to continue (y/n):\n");
+    scanf("%lf", &again);
+    if (again == 'y' || again == 'Y'){
+        run_code();
+    }
+    
+    else if (again == 'n' || again == 'N') {
+        return;
+    }
+
+
+
+
+
     return;
 
-
-
+    
 }
 
-int main () {
+int main()
+{
     run_code();
     return 0;
 }
